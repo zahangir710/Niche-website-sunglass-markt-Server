21c077d6a725afe0d4cd0b93c3f8ebc139cb0897
zahangir710 <zahangir710@gmail.com>
2021-11-15 18:03:16 +0100

    final commit

--- index.js
+++ index.js
@@ -122,13 +122,63 @@ async function run() {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.json(result);
-      console.log(result);
     });
     // GET review in Home page
     app.get("/review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.json(result);
+    });
+    // Make admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+
+      res.json(result);
+    });
+    // Load proaducts in Manage product page
+    app.get("/admin/products", async (req, res) => {
+      const result = await productCollection.find({}).toArray();
+      res.json(result);
+    });
+    // Delete a product by admin
+    app.delete("/admin/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
       console.log(result);
+      res.json(result);
+    });
+    // manage orders (get all orders)
+    app.get("/orders", async (req, res) => {
+      const orders = await orderCollection.find().toArray();
+      res.json(orders);
+      console.log(orders);
+    });
+    // Update order status
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const updatedStatus = req.body;
+      console.log("updated status", updatedStatus, updatedStatus.status);
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: { status: updatedStatus.status },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      console.log(result);
+      res.json(result);
+    });
+    // Cancel a Pending order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
     });
   } finally {
     // client.close();
